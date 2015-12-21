191d0e2bf621425a9feb156e40252c316584b0c7
Flaviu Silaghi <flaviu.silaghi@3pillarglobal.com>
2015-12-21 23:31:12 +0200

    First commit

---
+++ AniCalendar/AniCalendar/Model/API/AnilistAPI.swift
@@ -0,0 +1,13 @@
+//
+//  AnilistAPI.swift
+//  AniCalendar
+//
+//  Created by Flaviu Silaghi on 21/12/15.
+//  Copyright © 2015 3PillarGlobal. All rights reserved.
+//
+
+import UIKit
+
+class AnilistAPI: NSObject {
+
+}
